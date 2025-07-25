8b2fe8f1c5449b0a37cdaeaa66887c402ac3110b
eatiakoode <eati@akoode.in>
2025-07-25 15:17:44 +0530

    new server setup

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -14,6 +14,7 @@ if (typeof window !== "undefined") {
 
 export default function RootLayout({ children }) {
   return (
+  
     <html lang="en">
       <head>
         {/* <link
@@ -26,6 +27,33 @@ export default function RootLayout({ children }) {
         <link rel="icon"
          href={`${process.env.NEXT_PUBLIC_API_URL}public/assets/images/favicon.png`}
          />
+         {/* Meta Pixel script (head, before your code loads) */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1764659244930934');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+<noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1764659244930934&ev=PageView&noscript=1"
+        />
+      </noscript>
       </head>
       <body>
         <Provider store={store}>
@@ -38,5 +66,6 @@ export default function RootLayout({ children }) {
         </Provider>
       </body>
     </html>
+  
   );
 }
